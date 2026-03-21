2b1ae467ff7597b1dcf192ea4f58cadc56dae82f
wchange77 <20212010100@fudan.edu.cn>
2026-03-21 10:37:54 +0800

    Initial Commit

---
+++ handtyping/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  handtyping
+//
+//  Created by MacStudio on 2026/3/21.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+
+            ToggleImmersiveSpaceButton()
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
---
+++ handtyping/handtypingApp.swift
@@ -0,0 +1,33 @@
+//
+//  handtypingApp.swift
+//  handtyping
+//
+//  Created by MacStudio on 2026/3/21.
+//
+
+import SwiftUI
+
+@main
+struct handtypingApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
